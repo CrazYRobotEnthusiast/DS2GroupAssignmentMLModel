4f5fd047cabd2b5e7e4598874f1986906359622e
Akshit <akshitbhola2005@gmail.com>
2024-04-11 19:23:21 +0530

    Storing The Model With Data

---
+++ TheModel.py
@@ -0,0 +1,22 @@
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+import pandas as pd 
+import time
+
+data=pd.read_csv('TableFormModel.csv')
+features=['Year','GDPperCapita','OilPrice','Population','inflationRate','covidCases','unemploymentRate']
+y=data.TotalCars
+
+X=data[features]
+
+trainX,valX,trainy,valy=train_test_split(X,y,random_state=0)
+
+model=RandomForestRegressor(random_state=1)
+model.fit(trainX,trainy)
+predictions=model.predict(valX)
+mape = 100 * (abs(valy - predictions) / valy).mean()
+
+print("The Mean Absolute Percentage Error in model's predictions upon testing:", mape)
